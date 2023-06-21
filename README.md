863de5f2761338f591631fe6ccc7d26dce23f800
smallRG <douwan66@gmail.com>
2023-06-21 13:50:18 +0800

    First Trail:Include search function

---
+++ README.md
@@ -0,0 +1,6 @@
+1. This a interactive web application practice that allows users to view the restaurants information.
+2.It contains search bar funntion and popup forms to indicate detailed information of each restaurant.
+3.The simple web page:
+#Environment: Node.js/Bootstrap
+#Package:Express/Handlebars
+ 
--- app.js
+++ app.js
@@ -1,3 +1,36 @@
-const expree = require('express')
+const express = require('express')
 const app = express()
 const port = 3000
+const restaurantsList = require("./restaurant.json").results
+
+// require express-handlebars here
+const exphbs = require('express-handlebars')
+// setting template engine
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+//set static file
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+  res.render('index', { restaurantsInfo: restaurantsList })
+})
+
+app.get('/restaurants/:restaurantId', (req, res) => {
+  const inputId = req.params.restaurantId
+  const restaurantInfo = restaurantsList.find((data) => {
+    return data.id === Number(inputId)
+  })
+  res.render('show', { restaurantsInfo: restaurantInfo })
+})
+
+app.get('/search', (req, res) => {
+  const keyword = req.query.keywords.trim().toLowerCase()
+  const filteredContent = restaurantsList.filter((data) => {
+    return data.name.toLowerCase().includes(keyword) || data.category.includes(keyword)
+  })
+  res.render('index', { restaurantsInfo: filteredContent, keyword: keyword })
+})
+
+app.listen(port, () => {
+  console.log(`Listen on http://localhost:${port}`)
+})
